3abe9f4bb3bc0361d00dc5d1474c9cbd5578045c
ayan <mohdayanrehman07@gmail.com>
2024-06-03 23:38:36 +0530

    title-name-change

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "ayanrehman",
+  description: "This is my portfolio ayan rehman",
 };
 
 export default function RootLayout({
